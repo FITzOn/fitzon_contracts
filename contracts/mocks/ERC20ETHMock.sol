445c78d79d6ac78d749cfe90c552e3ee19c27a11
Jones Chi <jones.chi@tg3ds.com>
2022-06-20 18:29:49 +0800

    Add test cases for contracts
    1. Add test cases for contracts
    2. Rename to FITzOnWearable
    3. Change linter from solhint to ethlint

---
+++ contracts/mocks/ERC20ETHMock.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ERC20ETHMock is ERC20("ETHMock", "ETHM") {
+    function mint(address account, uint256 amount) public {
+        _mint(account, amount);
+    }
+}
