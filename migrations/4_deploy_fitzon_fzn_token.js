03b3ca4e8c61d0599f529e388dda73dbee55f4ca
Jones Chi <jones.chi@tg3ds.com>
2022-09-12 17:54:47 +0800

    Temporary remove deploy script for FIT/FZN

--- migrations/4_deploy_fitzon_fzn_token.js
+++
@@ -1,8 +0,0 @@
-const { deployProxy } = require('@openzeppelin/truffle-upgrades');
-const FITzOnFznToken = artifacts.require('FITzOnFznToken');
-
-module.exports = function (deployer, network) {
-  return deployer.then(async () => {
-    const fw = await deployProxy(FITzOnFznToken, ['FITzOnFZN', 'FZN', web3.utils.toWei('5000000000', 'ether')], { deployer });
-  });
-}
